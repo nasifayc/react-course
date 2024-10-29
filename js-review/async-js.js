e754faad9c926961efd15a4d0d5456f59ef0d173
nasifayc <nasifayc11@gmail.com>
2024-10-29 20:14:13 +0300

    fetching API data

--- js-review/async-js.js
+++ js-review/async-js.js
@@ -1,4 +1,13 @@
-// https://jsonplaceholder.typicode.com/todos/1
-fetch("https://jsonplaceholder.typicode.com/todos")
+// Method 1
+fetch("https://jsonplaceholder.typicode.com/todos/1")
   .then((res) => res.json())
   .then((data) => console.log(data));
+
+//   Method 2
+async function getData() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/todos/10");
+  const data = await res.json();
+  console.log(data);
+}
+
+getData();
