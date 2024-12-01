1a0639b6434db9855f9b73dad2a0cd8ecac576b3
nasifayc <nasifayc11@gmail.com>
2024-12-01 15:32:14 +0300

    feat: conditonal rendering

--- profile/src/data.js
+++ profile/src/data.js
@@ -1,5 +1,5 @@
 export const person = {
-  imageUrl: "/nasipic.jpg",
+  imageUrl: "/nasipic.png",
   name: "Jonas Schemedtmann",
   about:
     "Full-stack web developer and teacher at Udemy. When not coding or preparing a course, I like to play board games, to cook(and eat), or to just enjoy the Portuguese sun at the beach.",
