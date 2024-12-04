7acf288b589b38cda7d8dccf7613f1869fb2c848
nasifayc <nasifayc11@gmail.com>
2024-12-04 22:03:15 +0300

    feat: states

--- steps/src/App.js
+++ steps/src/App.js
@@ -8,6 +8,7 @@ const messages = [
 
 export default function App() {
   const [step, setStep] = useState(0);
+  const [isOpen, setOpen] = useState(true);
 
   const handlePreviousClick = () => {
     if (step > 0) {
@@ -24,29 +25,37 @@ export default function App() {
   };
 
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={step >= 0 ? "active" : ""}>1</div>
-        <div className={step >= 1 ? "active" : ""}> 2</div>
-        <div className={step >= 2 ? "active" : ""}>3</div>
-      </div>
-      <p className="message">
-        Step {step + 1}: {messages[step]}
-      </p>
-      <div className="buttons">
-        <button
-          style={{ backgroundColor: "#7950f2", color: "#fff" }}
-          onClick={handlePreviousClick}
-        >
-          Previous
-        </button>
-        <button
-          style={{ backgroundColor: "#7950f2", color: "#fff" }}
-          onClick={handleNextClick}
-        >
-          Next
-        </button>
-      </div>
-    </div>
+    <>
+      <button className="close" onClick={() => setOpen(!isOpen)}>
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 0 ? "active" : ""}>1</div>
+            <div className={step >= 1 ? "active" : ""}> 2</div>
+            <div className={step >= 2 ? "active" : ""}>3</div>
+          </div>
+          <p className="message">
+            Step {step + 1}: {messages[step]}
+          </p>
+          <div className="buttons">
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handlePreviousClick}
+            >
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handleNextClick}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+      ;
+    </>
   );
 }
